a583a5f4e00e36d78b7dc3b241dad145fa7bb451
shuai xia <shuai.xia@otms.cn>
2015-03-16 18:24:34 +0800

    Make the banner work on non-iOS devices.
    - Add test file
    - Add gulp task file to do some build tasks.

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// gulp plugins
+var gulp = require('gulp');
+var snippets = require('gulp-snippets');
+var minifycss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+
+// project variables
+var _base = 'jQuery.smartWebBanner.min';
+var _jsFile = _base + '.js';
+var _csssFile = _base + '.css';
+
+/**
+ * @task List all available tasks
+ */
+gulp.task('default', snippets.listTasks(gulp));
+
+/**
+ * @task Export target files
+ */
+gulp.task('dist', [ 'uglify-js', 'minify-css' ]);
+
+/**
+ * @task Minify javascript files
+ */
+gulp.task('uglify-js', function () {
+    gulp.src(_jsFile)
+        .pipe(uglify())
+        .pipe(rename({
+            basename: _base,
+            extname: '.js'
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+/**
+ * @task Minify css files
+ */
+gulp.task('minify-css', function () {
+    gulp.src(_csssFile)
+        .pipe(minifycss())
+        .pipe(rename({
+            basename: _base,
+            extname: '.css'
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+/**
+ * @task
+ */
+gulp.task('watch', function () {
+    gulp.watch('./*.js', [ 'uglify-js' ]);
+    gulp.watch('./*.css', [ 'minify-css' ]);
+});
